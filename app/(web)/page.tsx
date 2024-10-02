dc79f9f990c2e5341470b214dacd6154d5783d29
Bridger Tower <bridgertower@gmail.com>
2024-10-02 15:20:33 -0600

    Updated

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,9 +1,10 @@
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Link from "next/link";
 
 export default async function Index() {
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
-      {hasEnvVars ? <p>Sign Up Now</p> : <p>Welcome back!</p>}
+      <h1>Welcome to the web page</h1>
+      <Link href="/admin">Go to admin</Link>
     </main>
   );
 }
