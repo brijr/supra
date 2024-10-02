313f955920401d65343cf4a30310411c46f460e5
Bridger Tower <bridgertower@gmail.com>
2024-10-02 15:49:49 -0600

    Updated

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
 
 import { Main, Section, Container } from "@/components/craft";
+import Balancer from "react-wrap-balancer";
 
 export default async function Index() {
   return (
     <Main>
       <Section>
         <Container>
-          <h1>Welcome to the web page</h1>
-          <Link href="/admin">Go to admin</Link>
+          <h1>
+            <Balancer>
+              Welcome to the{" "}
+              <a
+                className="not-prose rounded-md font-normal px-2 bg-yellow-300 hover:bg-yellow-200"
+                href="https://github.com/brijr/supra"
+              >
+                brijr/supra
+              </a>
+              , a Next.js and Supabase Starter Kit
+            </Balancer>
+          </h1>
+          <div className="flex gap-2">
+            <Link href="/admin">Admin</Link>
+          </div>
         </Container>
       </Section>
     </Main>
