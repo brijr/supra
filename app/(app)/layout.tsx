f6ce4790a8017197740a7d57acf0d96cad17f7d1
Bridger Tower <bridgertower@gmail.com>
2024-10-02 16:31:39 -0600

    ui: spacing on app nav

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -40,7 +40,7 @@ const Sidebar = () => (
           <span className="sr-only">Toggle notifications</span>
         </Button>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 py-4">
         <AppNav />
       </div>
       <div className="mt-auto p-4">
@@ -91,18 +91,7 @@ const Header = () => (
         </div>
       </SheetContent>
     </Sheet>
-    <div className="w-full flex-1">
-      <form>
-        <div className="relative">
-          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            type="search"
-            placeholder="Search products..."
-            className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-          />
-        </div>
-      </form>
-    </div>
+    <div className="w-full flex-1"></div>
     <HeaderAuth />
   </header>
 );
