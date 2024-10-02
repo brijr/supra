2475a56aa213bf2ce706d5244a856ed4c0f8951e
Bridger Tower <bridgertower@gmail.com>
2024-10-02 15:07:28 -0600

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import "./globals.css";
+
 import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+
+import HeaderAuth from "@/components/header-auth";
 import Link from "next/link";
-import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -46,17 +48,6 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
                 <ThemeSwitcher />
               </footer>
             </div>
