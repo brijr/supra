e5f378586a363e14aa342c95075ec59b3b064d9d
Bridger Tower <bridgertower@gmail.com>
2024-10-02 15:41:50 -0600

    Updated

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 
 import { Main, Section, Container } from "@/components/craft";
+
 export default async function Index() {
   return (
-    <main className="flex-1 flex flex-col gap-6 px-4">
-      <h1>Welcome to the web page</h1>
-      <Link href="/admin">Go to admin</Link>
-    </main>
+    <Main>
+      <Section>
+        <Container>
+          <h1>Welcome to the web page</h1>
+          <Link href="/admin">Go to admin</Link>
+        </Container>
+      </Section>
+    </Main>
   );
 }
